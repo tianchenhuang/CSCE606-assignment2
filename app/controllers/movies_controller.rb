cb6606fd754fa568d89e72132b2d3787b7029569
Tianchen Huang <tianchenhuang@Tianchens-MacBook-Pro.local>
2024-02-18 18:41:32 -0600

    6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,23 +14,3 @@ class MoviesController < ApplicationController
 end
 
 
-# add a new movie
-
-def new
-  @movie = Movie.new
-end
-
-def create
-  @movie = Movie.new(movie_params)
-  if @movie.save
-    redirect_to movies_path, notice: 'Movie was successfully created.'
-  else
-    render :new
-  end
-end
-
-private
-
-def movie_params
-  params.require(:movie).permit(:title, :rating, :release_date)
-end
